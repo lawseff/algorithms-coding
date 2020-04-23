e3a76d0a76314ea46defc7daec34590dbbc123ce
Mikhail Loseu <Mikhail_Loseu@epam.com>
2020-04-23 15:28:47 +0300

    Change index searching algorithm in merge problem

--- java/merge/src/main/java/com/github/lawseff/merge/ListMerger.java
+++ java/merge/src/main/java/com/github/lawseff/merge/ListMerger.java
@@ -20,33 +20,16 @@ public class ListMerger {
     }
 
     private <T extends Comparable<T>> int findIndexToInsert(List<T> list, int begin, T value) {
-        int size = list.size();
-        if (size == 0) {
-            return 0;
-        }
-        if (isBiggerThanLast(list, value)) {
-            return size;
-        }
-        int l = begin;
-        int r = size - 1;
-        int m = 0;
-        while (l <= r) {
-            m = (l + r) / 2;
-            int comparisonResult = list.get(m).compareTo(value);
-            if (comparisonResult == 0) {
-                return m + 1; // adding 1 here because we don't need to overwrite the equal element
-            } else if (comparisonResult < 0) {
-                l = m + 1;
+        int l = begin, r = list.size();
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (list.get(m).compareTo(value) > 0) {
+                r = m;
             } else {
-                r = m - 1;
+                l = m + 1;
             }
         }
-        return m;
-    }
-
-    private <T extends Comparable<T>> boolean isBiggerThanLast(List<T> list, T value) {
-        T last = list.get(list.size() - 1);
-        return value.compareTo(last) > 0;
+        return l;
     }
 
 }
