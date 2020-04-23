44c1c0efddb748fe5374d31b0f203b82e02b7b3d
Mikhail Loseu <Mikhail_Loseu@epam.com>
2020-04-23 19:25:44 +0300

    Update horse-pawn algorithm

--- java/other/src/main/java/com/github/lawseff/other/Chess.java
+++ java/other/src/main/java/com/github/lawseff/other/Chess.java
@@ -3,8 +3,7 @@ package com.github.lawseff.other;
 public class Chess {
 
     public boolean canHorseBeatPawn(int horseX, int horseY, int pawnX, int pawnY) {
-        return Math.abs(horseX - pawnX) == 2 && Math.abs(horseY - pawnY) == 1 ||
-                Math.abs(horseX - pawnX) == 1 && Math.abs(horseY - pawnY) == 2;
+        return Math.abs(horseX - pawnX) * Math.abs(horseY - pawnY) == 2;
     }
 
 }
